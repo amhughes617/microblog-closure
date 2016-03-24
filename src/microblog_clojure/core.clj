f1e33282f331301c0d039bd6842572c0bf6cc42e
Alex <amhughes@g.cofc.edu>
2016-03-24 11:50:13 -0400

    save messages to disk, pull from disk to display them

--- src/microblog_clojure/core.clj
+++ src/microblog_clojure/core.clj
@@ -1,16 +1,40 @@
 (ns microblog-clojure.core
     (:require [compojure.core :as c]
-              [ring.adapter.jetty :as j])
-              
+              [ring.adapter.jetty :as j]
+              [ring.middleware.params :as p]
+              [ring.util.response :as r]
+              [hiccup.core :as h])             
   (:gen-class))
+
+(defonce server (atom nil))
+(defonce messages (atom []))
 
 (c/defroutes app
   (c/GET "/" request
-    "Hello, world!"))
+    (h/html [:html
+             [:body
+              [:form {:action "/add-message" :method "post"}
+                [:input {:type "text" :placeholder "Enter message" :name "message"}]
+                [:button {:type "submit"} "Add Message"]]
+              [:ol
+               (map (fn [message]
+                      [:li message])
+                 @messages)]]]))
+  (c/POST "/add-message" request
+    (let [message (get (:params request) "message")]
+      (swap! messages conj message)
+      (r/redirect "/"))))
 
-(defonce server (atom nil))
+(add-watch messages :save-to-disk
+  (fn [_ _ _ _]
+    (spit "messages.edn" (pr-str @messages))))
 
 (defn -main []
+  (try
+    (let [messages-str (slurp "messages.edn")
+          messages-vec (read-string messages-str)]
+      (reset! messages messages-vec))
+    (catch Exception _))
   (when @server
     (.stop @server))
-  (reset! server (j/run-jetty app {:port 3000 :join? false})))
+  (reset! server (j/run-jetty (p/wrap-params app) {:port 3000 :join? false})))
